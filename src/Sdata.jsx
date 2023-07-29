ce9abdf2024e4f9aa11ca96cd3993b0f003e0bc4
Rishabh Patidar <patidar123rishabh@gmail.com>
2023-07-29 15:33:24 +0530

    first commit

---
+++ src/Sdata.jsx
@@ -0,0 +1,36 @@
+import photo1 from "../src/image/car1.jpg"
+import photo2 from "../src/image/car3.jpg"
+import photo3 from "../src/image/bmw.jpg"
+import photo4 from "../src/image/lamborghini.webp"
+import photo5 from "../src/image/mercedies.jpeg"
+import photo6 from "../src/image/Rolls-Royce.webp"
+
+const Sdata=[
+    {
+        imgsrc: photo1,
+        title: "Ferrari",
+        
+    },
+    {
+        imgsrc: photo2,
+        title: "Jaguar"
+    }
+    , {
+        imgsrc: photo3,
+        title: "BMW"
+    }
+    , {
+        imgsrc: photo4,
+        title: "Lamborghini"
+    }
+    , {
+        imgsrc: photo5,
+        title: "Mercedes"
+    }
+    , {
+        imgsrc: photo6,
+        title: "Rolls-Royce"
+    }
+]
+
+export default Sdata;
